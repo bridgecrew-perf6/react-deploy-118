d43d2b26016731fcba8eabd867a6c65139936e82
kaal <kaalharper@protonmail.com>
2022-02-16 16:46:04 +0530

    adding tree

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
       Hello ,Welcome to this website
     </div>
-    
+    <Tree />
     </>
     
   );
---
+++ src/components/Footer/Tree/Tree.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Tree() {
+  return (
+    <div>
+
+        <select>
+            <option>Statistics</option>
+            <option>Aptitude</option>
+        </select>
+    </div>
+
+
+  )
+}
